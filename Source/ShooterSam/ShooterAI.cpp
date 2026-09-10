3d5d0cb2451d7378eb396491ff32d7b2abf7d02b
dinhtrong <80698862+TDTer@users.noreply.github.com>
2026-09-10 17:02:32 +0700

    Create AI Controller

---
+++ Source/ShooterSam/ShooterAI.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAI.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void AShooterAI::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (PlayerPawn)
+	{
+		SetFocus(PlayerPawn);
+
+
+	}
+}
+
+void AShooterAI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+	if (PlayerPawn)
+	{
+		MoveToActor(PlayerPawn, 200.0f);
+	}
+
+}
